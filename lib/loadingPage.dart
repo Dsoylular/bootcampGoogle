ced0b6a9d1859d531a2dee00903c589e039c1dce
Deniz Soylular <dsoylular22@ku.edu.tr>
2024-07-07 23:11:26 +0300

    loadingPage.dart Added It will be linked to login page in the future

---
+++ lib/loadingPage.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:bootcamp_google/MainPage.dart';
+import 'package:flutter/material.dart';
+
+import 'appColors.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => const MainPage(),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBlue,
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Pawdi',
+              style: TextStyle(
+                fontFamily: 'Chewy',
+                fontSize: 40,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bootcamp_google/loadingPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainPage(),
+      home: const LoadingScreen(),
     );
   }
 }
