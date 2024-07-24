7db2db588a3c60fa7afd4c17259cb23187bac518
Deniz Soylular <dsoylular22@ku.edu.tr>
2024-07-24 22:10:57 +0300

    app_colors.dart refactored.

--- lib/helperFiles/app_colors.dart
+++ lib/helperFiles/app_colors.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color white = Colors.white;
-Color black = Colors.black;
-Color darkGrey = Colors.black12;
-Color lightGrey = Colors.white30;
-Color denizCream = const Color.fromARGB(255, 245, 235, 230);
-
 Color pink = const Color.fromARGB(255, 218, 65, 103);
 Color cream = const Color.fromARGB(255, 245, 235, 230);
 Color darkBlue = const Color.fromARGB(255, 99, 105, 209);
 Color brown = const Color.fromARGB(255, 80, 36, 25);
 Color lightBlue = const Color.fromARGB(255, 145, 149, 222);
-
-Color darkCream = const Color.fromARGB(255, 225, 215, 210);
-
